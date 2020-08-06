453897e2c43e472170ba23aefb61a89c3578c3a9
IneegoMontoya <ByronJHSmith@Gmail.com>
2020-08-05 22:55:30 -0500

    First commit

---
+++ src/build/precache-manifest.60f2121a52f5b1dc94daac97a4678c2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2350bc1a3f99775cf5b510374e9ccc66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d2c9685bb5dde5d02ac",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "483028121d96a9d3202a",
+    "url": "/static/js/2.d4cc52d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4cc52d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2c9685bb5dde5d02ac",
+    "url": "/static/js/main.9ca5e1a6.chunk.js"
+  },
+  {
+    "revision": "13ff3f6c52d527a3b5c1",
+    "url": "/static/js/runtime-main.cf4e4ba0.js"
+  }
+]);
